7b985760fd474be29841522181df2a33c1153999
plmwa <townstgak@gmail.com>
2022-09-25 21:32:27 +0900

    ランキングとスコアのルーター設定

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
-from api.routers import user
+from api.routers import ranking, user,score
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(score.router)
+app.include_router(ranking.router)

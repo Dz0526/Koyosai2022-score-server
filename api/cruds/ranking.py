ccffc820bd0ed3115d359aee60db20a029ea7faf
plmwa <townstgak@gmail.com>
2022-10-10 23:19:57 +0900

    rankingのCRUD実装

--- api/cruds/ranking.py
+++ api/cruds/ranking.py
@@ -10,16 +10,56 @@ import logging
 
 
 
+async def get_rank_list(db:AsyncSession) -> List[Tuple[int,str,int]]:
+  
+    #一番高いレートを出す
+    #ユーザー1人1人の最高値レートを一度出す
+    return_rate_list=db.query(
+        score_model.Score.user_id,
+        func.max(score_model.Score.rate),
+        ).group_by(score_model.Score.user_id).all()
 
+    #一旦編集可能なlistに
+    rate_list=[]
+    for user_rate in return_rate_list:
+        rate_list.append(list(user_rate))
 
+    #name追加
+    user_name=""
+    for i in range(len(rate_list)):
+        user_name=db.query(user_model.User.name).filter(user_model.User.id == rate_list[i][0]).all()[0][0]
+        rate_list[i].append(user_name)
 
     
+    rate_list_sorted=sorted(rate_list,reverse=True,key=lambda x: x[1])
+    
 
-async def get_rank_list(db:AsyncSession, user_id: int) -> Tuple[int, str,int]:
+    #ランク算出
+    rate_list_sorted[0].append(1)
+    rank=0
+    for i in range(len(rate_list_sorted)):
+        rank=i+1
+        if i==0:
+            pass
+        elif rate_list_sorted[i][1]==rate_list_sorted[i-1][1]:
+            rank-=1
+            rate_list_sorted[i].append(rank)
+        else:
+            rate_list_sorted[i].append(rank)
+    
+    result=[]
+    for i in range(len(rate_list_sorted)):
+        dic_append={
+            "id":rate_list_sorted[i][0],
+            "name":rate_list_sorted[i][2],
+            "rate":rate_list_sorted[i][1],
+            "rank":rate_list_sorted[i][3],
+        }
+        result.append(dic_append)
+    return result
 
-    
-    
-    
+async def get_user_rank_list(db:AsyncSession, user_id: int) -> Tuple[int, str,int,int]:
+
     logger = logging.getLogger('uvicorn')    
     #一番高いレートを出す
     #ユーザー1人1人の最高値レートを一度出す
@@ -59,23 +99,45 @@ async def get_rank_list(db:AsyncSession, user_id: int) -> Tuple[int, str,int]:
             rate_list_sorted[i].append(rank)
         else:
             rate_list_sorted[i].append(rank)
+    
 
-    #first
-    first_list=[]
-    first_list.append(rate_list_sorted[0][0])
-    first_list.append(rate_list_sorted[0][1])
-    first_list.append(rate_list_sorted[0][2])
-    first_list.append(rate_list_sorted[0][3])
+    result={
+        "top_three": [],
+        "self": {
+            "id": 0,
+            "name": "伊藤大輝",
+            "rate": 0,
+            "rank": 0
+        },
+        "higher_around_rank_users": [],
+        "lower_around_rank_users": []
+    }
+
+    
+
+    #top_three
+    top_three_list=[]
+    top_three_list=rate_list_sorted[0:3]
+    for i in range(len(top_three_list)):
+        dic_append={
+            "id":top_three_list[i][0],
+            "name":top_three_list[i][2], 
+            "rate":top_three_list[i][1],
+            "rank":top_three_list[i][3]
+        }
+        result["top_three"].append(dic_append)
+        
+
 
     #self
-    self_list=[]
     self_index=0
     for i in range(len(rate_list_sorted)):
         if rate_list_sorted[i][0]==user_id:
-            self_list.append(rate_list_sorted[i][0])
-            self_list.append(rate_list_sorted[i][1])
-            self_list.append(rate_list_sorted[i][2])
-            self_list.append(rate_list_sorted[i][3])
+            result["self"]["id"]=rate_list_sorted[i][0]
+            result["self"]["name"]=rate_list_sorted[i][2]
+            result["self"]["rate"]=rate_list_sorted[i][1]
+            result["self"]["rank"]=rate_list_sorted[i][3]
+
             self_index=i
             break
     
@@ -85,7 +147,21 @@ async def get_rank_list(db:AsyncSession, user_id: int) -> Tuple[int, str,int]:
     if len(rate_list_sorted)<8:
         higher_around_rank_list=rate_list_sorted[1:self_index]
     else:
-        higher_around_rank_list=rate_list_sorted[self_index-4:self_index]
+        higher_around_rank_list=rate_list_sorted[self_index-5:self_index]
+    
+    logger.info(higher_around_rank_list)
+    
+
+    #resultに代入
+    for i in range(len(higher_around_rank_list)):
+        dic_append={
+            "id":higher_around_rank_list[i][0],
+            "name":higher_around_rank_list[i][2],                "rate":higher_around_rank_list[i][1],
+            "rank":higher_around_rank_list[i][3],
+        }
+        result["higher_around_rank_users"].append(dic_append)
+        
+    
 
     #lower_around_rank_users
     #selfより下5件取得
@@ -98,23 +174,16 @@ async def get_rank_list(db:AsyncSession, user_id: int) -> Tuple[int, str,int]:
     else:
         lower_around_rank_list=rate_list_sorted[self_index+1:self_index+6]
 
-    dic_higher_around_rank=[]
-    dic_lower_around_rank=[]
-    for i in higher_around_rank_list:
-        dic_higher_around_rank.append({*i})
-    for i in lower_around_rank_list:
-        dic_lower_around_rank.append({*i})
-        
-
-    result={"first":{*first_list},"self":{*self_list},"higher_around_rank_users":dic_higher_around_rank,"lower_around_rank_users":dic_lower_around_rank}
-    logger.info(first_list)
-    logger.info(self_list)
-    logger.info(higher_around_rank_list)
-    logger.info(lower_around_rank_list)
-    logger.info(rate_list_sorted)
-
-    #logger.info(result)
-
     
+    #lower result代入
+    for i in range(len(lower_around_rank_list)):
+        dic_append={
+            "id":lower_around_rank_list[i][0],
+            "name":lower_around_rank_list[i][2],
+            "rate":lower_around_rank_list[i][1],
+            "rank":lower_around_rank_list[i][3],
+        }
+        result["lower_around_rank_users"].append(dic_append)
 
+    logger.info(result)
     return result
